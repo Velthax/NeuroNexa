afaadc42e337b1542fd93dfb0dfb5ec99a1bc687
Vaidik <crazyphysicist999@gmail.com>
2025-07-20 15:09:00 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',             // Enables static export
-  trailingSlash: true,         // Ensures directories work like folders (Netlify-friendly)
-  images: {
-    unoptimized: true,         // Disables image optimization for static export compatibility
-  },
-  reactStrictMode: true,       // (Optional) Enables extra checks during development
+  output: 'export',
 };
 
 module.exports = nextConfig;
