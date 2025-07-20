c83ea958d21269af9c431058433168ad8a775176
Vaidik <crazyphysicist999@gmail.com>
2025-07-20 16:14:49 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'radial': 'radial-gradient(ellipse at top, var(--tw-gradient-stops))'
-      },
-      animation: {
-        pulse: 'pulse 4s ease-in-out infinite',
-      },
-      fontFamily: {
-        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
