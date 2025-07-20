e2e318e51395cb5e1cadda0f452f8d77fd9227c1
Vaidik <crazyphysicist999@gmail.com>
2025-07-20 16:15:14 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // enables static export
   reactStrictMode: true,
-  images: {
-    unoptimized: true, // Required for static export
-  },
-  trailingSlash: true, // Helps prevent 404s in static hosting like Netlify
+  trailingSlash: true, // required for Netlify routing
 };
 
 module.exports = nextConfig;
