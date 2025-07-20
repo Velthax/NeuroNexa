7bdd3979d3b56a7f996bd7f4aa8a158ae6758358
Vaidik <crazyphysicist999@gmail.com>
2025-07-20 14:54:30 +0530

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '20vh' }}>
+      <h1>🚀 Welcome to NeuroNexa</h1>
+      <p>Where Thought Becomes Command</p>
+    </div>
+  );
+}
